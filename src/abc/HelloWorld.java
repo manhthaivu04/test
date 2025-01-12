1bf76336b3808ced95bb3f4d4f4d93152ea4644d
manhthaivu04 <vumanhthai1029@gmail.com>
2025-01-13 00:31:43 +0700

    init project

---
+++ src/abc/HelloWorld.java
@@ -0,0 +1,10 @@
+package abc;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and Thai");
+		System.out.println("Hello world with java and Thai");
+		System.out.println("Hello world with java and Thai");
+		System.out.println("Hello world with java and Thai");
+	}
+}
